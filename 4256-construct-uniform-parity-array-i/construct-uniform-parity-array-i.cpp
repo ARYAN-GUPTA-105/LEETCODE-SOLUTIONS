c6d929d49d29729a91a8000a799c7b58b88c6bdb
Aryan Gupta <sg7084108@gmail.com>
2026-09-02 11:43:19 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (67.80%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+using vi = vector<int>;
+using vll = vector<long long>;
+using vvll = vector<vll>;
+using vvii = vector<vector<int>>;
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = a; i >= n; i--)
+#define pb push_back
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
