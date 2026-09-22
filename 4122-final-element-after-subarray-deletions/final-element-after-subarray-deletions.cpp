4149d8ec8ae2dc7824a2aef8af5c39a67c80c19b
Aryan Gupta <sg7084108@gmail.com>
2026-09-22 11:48:19 +0530

    Time: 0 ms (100.00%) | Memory: 179.8 MB (57.14%) - LeetSync

---
+++ 4122-final-element-after-subarray-deletions/final-element-after-subarray-deletions.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+using vi = vector<int>;
+using vll = vector<long long>;
+using vvll = vector<vll>;
+using vvii = vector<vector<int>>;
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = a; i >= n; i--)
+#define pb push_back
+#define um unordered_map
+#define us unordered_set
+class Solution {
+public:
+    int finalElement(vector<int>& nums) {
+        ll n = nums.size();
+        return max(nums[0],nums[n-1]);
+    }
+};
